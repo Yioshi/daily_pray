5022c68d77e903146409cc27b5b25cefac66d2bb
Yioshi <martynf88@gmail.com>
2022-04-10 15:48:55 +0100

    - Configured and created helpers - Change include_helpers config to false - Split the _collapsible_elements.html.erb file's cdontent into partials and extract logic from the file into partials

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module DailyPray
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    # To not include all helpers automatically
+    config.action_controller.include_all_helpers = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
